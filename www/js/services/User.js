3ff868560e974bcc16c8816069f546fb23b40f20
risan <risanbagja@gmail.com>
2015-10-22 13:16:58 +0200

    Use webstorage to store user information.

--- www/js/services/User.js
+++ www/js/services/User.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('app.Services').service('User', ['Google', function(Google) {
+angular.module('app.Services').service('User', ['$window', 'Google', function($window, Google) {
   var user = null;
 
   this.login = function(successCallback, errorCallback) {
     Google.getAccessToken(function(accessToken) {
       Google.getUser(accessToken, function(userData) {
         user = userData;
+        $window.localStorage.setItem('user', angular.toJson(user));
         successCallback(user);
       }, function(error) {
         errorCallback(error.message);
@@ -18,10 +19,15 @@ angular.module('app.Services').service('User', ['Google', function(Google) {
 
   this.logout = function() {
     user = null;
+    $window.localStorage.removeItem('user');
     return this;
   }
 
   this.get = function() {
+    if (user === null && $window.localStorage.getItem('user')) {
+      user = angular.fromJson($window.localStorage.getItem('user'));
+    }
+
     return user;
   };
 }]);
